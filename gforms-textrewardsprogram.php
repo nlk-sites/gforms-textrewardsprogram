d1548a3f56ed90dd1930722a5d461af7b3e9ae3d
alex chousmith <alex@ninthlink.com>
2016-04-25 16:27:42 -0700

    GWC-79 oops rename php er

--- gforms-textrewardsprogram.php
+++ gforms-textrewardsprogram.php
@@ -621,7 +621,7 @@ if (class_exists("GFForms")) {
                         if ( $l[2] == 'state' ) {
                           if ( strlen( $entry[ $v ] ) > 2 ) {
                             // convert to abbrev or '' if not abbrevable
-                            $jsonArray[ $l[2] ] = textrewardsprogram_stateabbr( $entry[ $v ] );
+                            $jsonArray[ $l[2] ] = gforms_textrewardsprogram_stateabbr( $entry[ $v ] );
                           }
                         }
                     endif;
@@ -672,63 +672,6 @@ if (class_exists("GFForms")) {
          *
          **/
          
-        // abbrev states, returning 2-letter or nothing ''
-        public function textrewardsprogram_stateabbr( $st ) {
-          $states = array(
-            'Alabama'=>'AL',
-            'Alaska'=>'AK',
-            'Arizona'=>'AZ',
-            'Arkansas'=>'AR',
-            'California'=>'CA',
-            'Colorado'=>'CO',
-            'Connecticut'=>'CT',
-            'Delaware'=>'DE',
-            'Florida'=>'FL',
-            'Georgia'=>'GA',
-            'Hawaii'=>'HI',
-            'Idaho'=>'ID',
-            'Illinois'=>'IL',
-            'Indiana'=>'IN',
-            'Iowa'=>'IA',
-            'Kansas'=>'KS',
-            'Kentucky'=>'KY',
-            'Louisiana'=>'LA',
-            'Maine'=>'ME',
-            'Maryland'=>'MD',
-            'Massachusetts'=>'MA',
-            'Michigan'=>'MI',
-            'Minnesota'=>'MN',
-            'Mississippi'=>'MS',
-            'Missouri'=>'MO',
-            'Montana'=>'MT',
-            'Nebraska'=>'NE',
-            'Nevada'=>'NV',
-            'New Hampshire'=>'NH',
-            'New Jersey'=>'NJ',
-            'New Mexico'=>'NM',
-            'New York'=>'NY',
-            'North Carolina'=>'NC',
-            'North Dakota'=>'ND',
-            'Ohio'=>'OH',
-            'Oklahoma'=>'OK',
-            'Oregon'=>'OR',
-            'Pennsylvania'=>'PA',
-            'Rhode Island'=>'RI',
-            'South Carolina'=>'SC',
-            'South Dakota'=>'SD',
-            'Tennessee'=>'TN',
-            'Texas'=>'TX',
-            'Utah'=>'UT',
-            'Vermont'=>'VT',
-            'Virginia'=>'VA',
-            'Washington'=>'WA',
-            'West Virginia'=>'WV',
-            'Wisconsin'=>'WI',
-            'Wyoming'=>'WY'
-          );
-          // return the abbreviation or nothing
-          return isset( $states[ $st ] ) ? $states[$st] : '';
-        }
 
         // Debug builder
         public function avala_debug()
@@ -1088,4 +1031,63 @@ function gforms_textrewardsprogram_tabindexer( $tab_index, $form = false ) {
     if( $form )
         add_filter( 'gform_tabindex_' . $form['id'], 'gforms_textrewardsprogram_tabindexer' );
     return GFCommon::$tab_index >= $starting_index ? GFCommon::$tab_index : $starting_index;
+}
+
+
+// abbrev states, returning 2-letter or nothing ''
+function gforms_textrewardsprogram_stateabbr( $st ) {
+  $states = array(
+    'Alabama'=>'AL',
+    'Alaska'=>'AK',
+    'Arizona'=>'AZ',
+    'Arkansas'=>'AR',
+    'California'=>'CA',
+    'Colorado'=>'CO',
+    'Connecticut'=>'CT',
+    'Delaware'=>'DE',
+    'Florida'=>'FL',
+    'Georgia'=>'GA',
+    'Hawaii'=>'HI',
+    'Idaho'=>'ID',
+    'Illinois'=>'IL',
+    'Indiana'=>'IN',
+    'Iowa'=>'IA',
+    'Kansas'=>'KS',
+    'Kentucky'=>'KY',
+    'Louisiana'=>'LA',
+    'Maine'=>'ME',
+    'Maryland'=>'MD',
+    'Massachusetts'=>'MA',
+    'Michigan'=>'MI',
+    'Minnesota'=>'MN',
+    'Mississippi'=>'MS',
+    'Missouri'=>'MO',
+    'Montana'=>'MT',
+    'Nebraska'=>'NE',
+    'Nevada'=>'NV',
+    'New Hampshire'=>'NH',
+    'New Jersey'=>'NJ',
+    'New Mexico'=>'NM',
+    'New York'=>'NY',
+    'North Carolina'=>'NC',
+    'North Dakota'=>'ND',
+    'Ohio'=>'OH',
+    'Oklahoma'=>'OK',
+    'Oregon'=>'OR',
+    'Pennsylvania'=>'PA',
+    'Rhode Island'=>'RI',
+    'South Carolina'=>'SC',
+    'South Dakota'=>'SD',
+    'Tennessee'=>'TN',
+    'Texas'=>'TX',
+    'Utah'=>'UT',
+    'Vermont'=>'VT',
+    'Virginia'=>'VA',
+    'Washington'=>'WA',
+    'West Virginia'=>'WV',
+    'Wisconsin'=>'WI',
+    'Wyoming'=>'WY'
+  );
+  // return the abbreviation or nothing
+  return isset( $states[ $st ] ) ? $states[$st] : '';
 }
