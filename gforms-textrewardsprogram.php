77bca83e93d2ca973a7a60c9038f0a5a1f435336
alex chousmith <alex@ninthlink.com>
2016-04-25 15:44:21 -0700

    GWC-84 fix state abbrevs to 2 letters the rest is good

--- gforms-textrewardsprogram.php
+++ gforms-textrewardsprogram.php
@@ -617,6 +617,13 @@ if (class_exists("GFForms")) {
                 if ( $l[0] == 'avalaMappedFields' ) {
                     if ( array_key_exists( $l[2], $jsonArray ) && !empty( $v ) ) :
                         $jsonArray[ $l[2] ] = $entry[ $v ];
+                        // double check STATE
+                        if ( $l[2] == 'state' ) {
+                          if ( strlen( $entry[ $v ] ) > 2 ) {
+                            // convert to abbrev or '' if not abbrevable
+                            $jsonArray[ $l[2] ] = textrewardsprogram_stateabbr( $entry[ $v ] );
+                          }
+                        }
                     endif;
                 }
             }
@@ -664,6 +671,64 @@ if (class_exists("GFForms")) {
          *  Useful functions for parsing data, formatting, etc.
          *
          **/
+         
+        // abbrev states, returning 2-letter or nothing ''
+        public function textrewardsprogram_stateabbr( $st ) {
+          $states = array(
+            'Alabama'=>'AL',
+            'Alaska'=>'AK',
+            'Arizona'=>'AZ',
+            'Arkansas'=>'AR',
+            'California'=>'CA',
+            'Colorado'=>'CO',
+            'Connecticut'=>'CT',
+            'Delaware'=>'DE',
+            'Florida'=>'FL',
+            'Georgia'=>'GA',
+            'Hawaii'=>'HI',
+            'Idaho'=>'ID',
+            'Illinois'=>'IL',
+            'Indiana'=>'IN',
+            'Iowa'=>'IA',
+            'Kansas'=>'KS',
+            'Kentucky'=>'KY',
+            'Louisiana'=>'LA',
+            'Maine'=>'ME',
+            'Maryland'=>'MD',
+            'Massachusetts'=>'MA',
+            'Michigan'=>'MI',
+            'Minnesota'=>'MN',
+            'Mississippi'=>'MS',
+            'Missouri'=>'MO',
+            'Montana'=>'MT',
+            'Nebraska'=>'NE',
+            'Nevada'=>'NV',
+            'New Hampshire'=>'NH',
+            'New Jersey'=>'NJ',
+            'New Mexico'=>'NM',
+            'New York'=>'NY',
+            'North Carolina'=>'NC',
+            'North Dakota'=>'ND',
+            'Ohio'=>'OH',
+            'Oklahoma'=>'OK',
+            'Oregon'=>'OR',
+            'Pennsylvania'=>'PA',
+            'Rhode Island'=>'RI',
+            'South Carolina'=>'SC',
+            'South Dakota'=>'SD',
+            'Tennessee'=>'TN',
+            'Texas'=>'TX',
+            'Utah'=>'UT',
+            'Vermont'=>'VT',
+            'Virginia'=>'VA',
+            'Washington'=>'WA',
+            'West Virginia'=>'WV',
+            'Wisconsin'=>'WI',
+            'Wyoming'=>'WY'
+          );
+          // return the abbreviation or nothing
+          return isset( $states[ $st ] ) ? $states[$st] : '';
+        }
 
         // Debug builder
         public function avala_debug()
